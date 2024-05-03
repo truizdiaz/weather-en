ab6b2c0fddf514727b78209a4e4f542e633615b2
Tomas Ruiz Diaz <external.diaz_tomas-ruiz@allianzdirect.de>
2024-05-03 14:35:49 +0200

    second part

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { FormsModule } from '@angular/forms';
+import { DecimalPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, DecimalPipe, NgIf],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  city: string = '';
+  temperature: number = 0;
+  humedity: number = 0;
+  weather: string = '';
+  query: boolean = false;
+  error: boolean = false;
+
+  constructor(private weatherService: WeatherService) {
+
+  }
+
+  getWeather() {
+
+    this.weatherService.getWeather(this.city).subscribe({
+      next: (data) => {
+        this.query = true
+        this.temperature = data.main.temp - 272;
+        this.humedity = data.main.humidity;
+        this.weather = data.weather[0].main;
+      },
+      error: (e) => {
+        this.error = true;
+        this.city = '';
+
+        setTimeout(() => {
+          this.error = false;
+        }, 3000);
+      }
+    })
+  }
+
+
 
 }
